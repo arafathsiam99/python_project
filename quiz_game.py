5792f822586932dc5380a7b576730120849228f2
arafathsiam99 <siammdarafathislam@gmail.com>
2024-12-30 11:25:59 +0600

    quiz game updated

--- quiz_game.py
+++ quiz_game.py
@@ -1,4 +1,32 @@
 print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
-print(playing)
+
+if playing != "yes":
+    quit()
+
+print("Ok! Let's play :)")
+
+answer = input("What does CPU stand for? ")
+if answer == "Central Processing Unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer == "Graphics Processing Unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer == "Random Access Memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer == "Power Supply Unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
