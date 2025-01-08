b2cedada6c0ed7c44939541359568a96b53d7a6c
arafathsiam99 <siammdarafathislam@gmail.com>
2025-01-08 11:44:10 +0600

    update quiz code

--- quiz_game.py
+++ quiz_game.py
@@ -2,31 +2,39 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Ok! Let's play :)")
+score = 0
 
 answer = input("What does CPU stand for? ")
-if answer == "Central Processing Unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does GPU stand for? ")
-if answer == "Graphics Processing Unit":
+if answer.lower() == "graphics processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for? ")
-if answer == "Random Access Memory":
+if answer.lower() == "random access memory":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does PSU stand for? ")
-if answer == "Power Supply Unit":
+if answer.lower() == "power supply unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+print("You got " + str(score) + "questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
